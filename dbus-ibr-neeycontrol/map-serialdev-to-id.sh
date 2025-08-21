58be4313f55d7493557c99f5f19a4e1dce6a10fe
ErwinRieger <erwin.rieger@ibrieger.de>
2025-08-21 19:34:38 +0200

    Fix dbus-ibr-neeycontrol/map-serialdev-to-id.sh, venus-os udevadm has no "--property" cli option.

--- dbus-ibr-neeycontrol/map-serialdev-to-id.sh
+++ dbus-ibr-neeycontrol/map-serialdev-to-id.sh
@@ -5,7 +5,8 @@ echo "# Map serial devices to their udev ID_SERIAL_SHORT" > $mapfile
 echo "SerialToId = {" >> $mapfile
 
 for dev in $(ls /dev/ttyUSB*); do
-    serialshort="$(udevadm info -q property --property ID_SERIAL_SHORT ${dev}|cut -d'=' -f2)"
+    # serialshort="$(udevadm info -q property --property ID_SERIAL_SHORT ${dev}|cut -d'=' -f2)"
+    serialshort="$(udevadm info -q property ${dev}|grep ID_SERIAL_SHORT|cut -d'=' -f2)"
     echo "    \"${dev}\": \"${serialshort}\"," >> $mapfile
 done
 
