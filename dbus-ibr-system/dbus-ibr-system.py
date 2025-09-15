dae2bf200ae512d03accbf33a92844da811f9a8c
ErwinRieger <erwin.rieger@ibrieger.de>
2025-09-15 14:28:18 +0200

    Fix aiovelib import.

--- dbus-ibr-system/dbus-ibr-system.py
+++ dbus-ibr-system/dbus-ibr-system.py
@@ -16,7 +16,7 @@ except ImportError:
     from dbus_next.constants import BusType
 
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), '..', 'aiovelib'))
-sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/data', 'etc', 'ibr-venus-services', 'aiovelib'))
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/data', 'ibr-venus-services', 'aiovelib'))
 
 from aiovelib.service import Service as AioDbusService
 from aiovelib.service import IntegerItem, TextItem, DoubleItem
