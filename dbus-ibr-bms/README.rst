cc0914a447819a67168818f676f71ecf3f0af1c4
ErwinRieger <erwin.rieger@ibrieger.de>
2025-09-21 11:28:00 +0200

    Import.

---
+++ dbus-ibr-bms/README.rst
@@ -0,0 +1,9 @@
+
+Venus-os dbus battery aggregator/BMS
+====================================
+
+Victron venus-os service to aggregate multiple batteries into one virtual battery.
+Implements a DVCC algorithm, too.
+
+
+
---
+++ dbus-ibr-bms/config.py
@@ -0,0 +1,20 @@
+
+# Expected number of battery banks.
+NUMBER_OF_BATTERIES = 1
+
+# Battery capacity [Ah]
+BATTERY_CAPACITY = (100*0.9)
+
+# Time to wait after cells are balanced to
+# enter floating state 
+BALANCETIME = 5 * 60 # [s]
+
+#
+# Import loal settings
+#
+try:
+    from config.local import *
+except ModuleNotFoundError:
+    pass
+
+
