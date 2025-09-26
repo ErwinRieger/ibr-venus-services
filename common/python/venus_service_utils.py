0b44fcc4e236b5b322e5aa23ac475dcb97b7393c
ErwinRieger <erwin.rieger@ibrieger.de>
2025-09-26 09:44:01 +0200

    Fixed indentation.

--- common/python/venus_service_utils.py
+++ common/python/venus_service_utils.py
@@ -2,11 +2,11 @@
 
 
 def bound(low, v, high):
-	return max(low, min(v, high))
+    return max(low, min(v, high))
 
 def saveAvg(l):
 
     if l:
         return sum(l) / len(l)
-	return 0
+    return 0
 
