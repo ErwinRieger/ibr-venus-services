5afe876edd90bbcf44d8fee4a7bac05c235f3424
ErwinRieger <erwin.rieger@ibrieger.de>
2025-10-27 14:18:14 +0100

    refactor(neeycontrol): Use BattInfo from DBus and utils function
    - Refactor `dbus-neeycontrol` to get battery information from the
      `/Info/BattInfo` DBus path provided by `dbus-ibr-system`.
    - This removes the dependency on the static configuration files
      `map_serialdev_to_id.py` and `map_id_to_btmac.py`.
    - A new generic function `parse_batt_info` is added to
      `venus_service_utils.py` to parse the `BattInfo` string array.
    - `dbus-neeycontrol` is updated to use this function and to
      dynamically set up the balancers based on the information from DBus.
    - Fixes a bug where removed balancers were not cleared from the
      internal state.

--- common/python/venus_service_utils.py
+++ common/python/venus_service_utils.py
@@ -1,4 +1,24 @@
+import logging
 
+def parse_batt_info(batt_info):
+    """
+    Parses the flat list of battery info from /Info/BattInfo on DBus.
+    Returns a dictionary with device name as key and a tuple (devname, btmac) as value.
+    """
+    batts = {}
+    if not batt_info:
+        return batts
+
+    logging.info(f"Parsing BattInfo: {batt_info}")
+    for i in range(0, len(batt_info), 3):
+        # batt_info is a flat list of [dev, devname, btmac, ...]
+        if i + 2 < len(batt_info):
+            dev, devname, btmac = batt_info[i:i+3]
+            batts[dev] = (devname, btmac)
+        else:
+            logging.warning(f"Incomplete entry in BattInfo: {batt_info[i:]}")
+
+    return batts
 
 
 def bound(low, v, high):
@@ -9,4 +29,3 @@ def saveAvg(l):
     if l:
         return sum(l) / len(l)
     return 0
-
