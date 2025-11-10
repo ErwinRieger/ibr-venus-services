d7d79fb2e06a56c90e6529337298a1d25b27b989
ErwinRieger <erwin.rieger@ibrieger.de>
2025-11-11 00:14:10 +0100

    map-serialdev-to-id.sh: Use ID_SERIAL if ID_SERIAL_SHORT is not available.

--- dbus-ibr-system/map-serialdev-to-id.sh
+++ dbus-ibr-system/map-serialdev-to-id.sh
@@ -6,8 +6,11 @@ echo "# Map serial devices to their udev ID_SERIAL_SHORT" > $mapfile
 echo "SerialToId = {" >> $mapfile
 
 for dev in $(ls /dev/ttyUSB*); do
-    # serialshort="$(udevadm info -q property --property ID_SERIAL_SHORT ${dev}|cut -d'=' -f2)"
     serialshort="$(udevadm info -q property ${dev}|grep ID_SERIAL_SHORT|cut -d'=' -f2)"
+    # Use ID_SERIAL if ID_SERIAL_SHORT is not available
+    if [ -z "$serialshort" ]; then
+        serialshort="$(udevadm info -q property ${dev}|grep ID_SERIAL|cut -d'=' -f2)"
+    fi
     echo "    \"${dev}\": \"${serialshort}\"," >> $mapfile
 done
 
